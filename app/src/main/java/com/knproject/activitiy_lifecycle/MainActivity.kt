f1b13fb405a4aea12b6f52e70ed72fd7fecc2bb4
doanthekhoinguyen <doanthekhoinguyen.dev@gmail.com>
2023-06-22 14:12:26 +0700

    on restart on stop

--- app/src/main/java/com/knproject/activitiy_lifecycle/MainActivity.kt
+++ app/src/main/java/com/knproject/activitiy_lifecycle/MainActivity.kt
@@ -58,6 +58,23 @@ class MainActivity : AppCompatActivity() {
         }
 
     }
+
+    override fun onStop() {
+        super.onStop()
+        if(running){
+            saveOffset()
+            stopwatch.stop()
+        }
+    }
+
+    override fun onRestart() {
+        super.onRestart()
+        if(running){
+            setBaseTime()
+            stopwatch.start()
+            offset = 0
+        }
+    }
     //update the stopwatch base time
     fun setBaseTime(){
         stopwatch.base =SystemClock.elapsedRealtime()-offset
