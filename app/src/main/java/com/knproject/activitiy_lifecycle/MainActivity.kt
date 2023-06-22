95907dbce20bc0908d8e2ca9bd8658fedf82b9b1
doanthekhoinguyen <doanthekhoinguyen.dev@gmail.com>
2023-06-22 13:48:58 +0700

    saveInstanceState

--- app/src/main/java/com/knproject/activitiy_lifecycle/MainActivity.kt
+++ app/src/main/java/com/knproject/activitiy_lifecycle/MainActivity.kt
@@ -13,12 +13,28 @@ class MainActivity : AppCompatActivity() {
     lateinit var stopwatch: Chronometer
     var running = false
     var offset: Long = 0
+
+    val OFFSET_KEY = "offset"
+    val RUNNING_KEY = "running"
+    val BASE_KEY="base"
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
         //get a reference
         stopwatch = findViewById<Chronometer>(R.id.stop_watch)
+
+        //restore the previous state
+        if(savedInstanceState != null){
+            offset = savedInstanceState.getLong(OFFSET_KEY)
+            running = savedInstanceState.getBoolean(RUNNING_KEY)
+
+            if(running){
+                stopwatch.base = savedInstanceState.getLong(BASE_KEY)
+                stopwatch.start()
+            } else setBaseTime()
+
+        }
 
         //start button starts the stopwatch if it's not running
         val startButton = findViewById<Button>(R.id.start_button)
@@ -49,5 +65,13 @@ class MainActivity : AppCompatActivity() {
     //record the offset
     fun saveOffset(){
         offset = SystemClock.elapsedRealtime()-stopwatch.base
+    }
+
+    override fun onSaveInstanceState(savedInstanceState: Bundle) {
+        savedInstanceState.putLong(OFFSET_KEY,offset)
+        savedInstanceState.putBoolean(RUNNING_KEY,running)
+        savedInstanceState.putLong(BASE_KEY,stopwatch.base)
+        super.onSaveInstanceState(savedInstanceState)
+
     }
 }
